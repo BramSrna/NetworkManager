4317bfc3e3894fcc0624233f7c269be03ea8f454
BramSrna <bramsrna@cmail.carleton.ca>
2022-05-12 20:28:43 -0400

    Initialize documentation and add sensor listeners

--- control_centre/src/control_centre.py
+++ control_centre/src/control_centre.py
@@ -12,26 +12,39 @@ class ControlCentre(HttpCommunicator):
         self.swarm_bots = []
         self.run_logs = {}
 
+        self.memory = {}
+
     def get_id(self):
         return self.id
 
     def add_swarm_bot(self, new_swarm_bot):
         if not isinstance(new_swarm_bot, SwarmBot):
             raise Exception("ERROR: new_swarm_bot must be of type SwarmBot")
-        self.swarm_bots.append(new_swarm_bot.get_id())
+        bot_id = new_swarm_bot.get_id()
+        self.swarm_bots.append(bot_id)
 
-    def delegate_task(self, swarm_bot_id, task, task_iterations):
-        self.send_message(swarm_bot_id, MessageTypes.NEW_TASK, {"TASK": task, "ITERATIONS": task_iterations})
+    def define_data_flow(self, swarm_bot_id, sensor_id, data_flow):
+        self.send_message(swarm_bot_id, MessageTypes.NEW_DATA_FLOW, {"SENSOR_ID": sensor_id, "DATA_FLOW": data_flow})
 
     def send_message(self, target_bot_id, message_type, message_payload):
         self.http_mock.send_message(self.id, target_bot_id, message_type, message_payload)
 
     def receive_message(self, sender_id, message_type, message_payload):
-        if message_type == MessageTypes.RUN_LOGS:
-            self.run_logs[sender_id] = message_payload
+        if message_type == MessageTypes.SENSOR_VAL:
+            sender_id = sender_id
+            sensor_id = message_payload["SENSOR_ID"]
+            data = message_payload["DATA"]
+            if sender_id not in self.memory:
+                self.memory[sender_id] = {}
+            if sensor_id not in self.memory[sender_id]:
+                self.memory[sender_id][sensor_id] = []
+            self.memory[sender_id][sensor_id].append(data)
         else:
             raise Exception("ERROR: Unknown or undandled message type: " + str(message_type))
 
     def get_run_logs(self, swarm_bot_id):
         self.send_message(swarm_bot_id, MessageTypes.LOG_REQUEST, {})
         return self.run_logs[swarm_bot_id]
+
+    def read_from_memory(self, swarm_bot_id, sensor_id):
+        return self.memory[swarm_bot_id][sensor_id]
--- swarm_bot/src/swarm_bot.py
+++ swarm_bot/src/swarm_bot.py
@@ -1,5 +1,6 @@
 from common.src.message_types import MessageTypes
 from http_mock.src.http_communicator import HttpCommunicator
+from random import randint
 
 
 class SwarmBot(HttpCommunicator):
@@ -12,6 +13,8 @@ class SwarmBot(HttpCommunicator):
         self.curr_task = None
         self.run_logs = []
 
+        self.sensors = {}
+
     def get_id(self):
         return self.id
 
@@ -20,15 +23,25 @@ class SwarmBot(HttpCommunicator):
             self.run_logs.append(self.curr_task())
 
     def receive_message(self, sender_id, message_type, message_payload):
-        if message_type == MessageTypes.NEW_TASK:
-            new_task = message_payload["TASK"]
-            num_iterations = message_payload["ITERATIONS"]
-            self.curr_task = new_task
-            self.execute_curr_task(num_iterations)
-        elif message_type == MessageTypes.LOG_REQUEST:
-            self.send_message(sender_id, MessageTypes.RUN_LOGS, self.run_logs)
+        if message_type == MessageTypes.NEW_DATA_FLOW:
+            sensor_id = message_payload["SENSOR_ID"]
+            data_flow = message_payload["DATA_FLOW"]
+            self.sensors[sensor_id] = data_flow
         else:
             raise Exception("ERROR: Unknown or undandled message type: " + str(message_type))
 
     def send_message(self, target_bot_id, message_type, message_payload):
         self.http_mock.send_message(self.id, target_bot_id, message_type, message_payload)
+
+    def add_sensor(self, sensor_id):
+        self.sensors[sensor_id] = []
+
+    def read_from_sensor(self, sensor_id):
+        new_val = randint(1, 10)
+        data_flow = self.sensors[sensor_id]
+        for id in data_flow:
+            if id == self.id:
+                self.memory.append(new_val)
+            else:
+                self.send_message(id, MessageTypes.SENSOR_VAL, {"SENSOR_ID": sensor_id, "DATA": new_val})
+        return new_val
