837258a437451d5b4656e00a6f90bb6d3a93fbea
BramSrna <bramsrna@cmail.carleton.ca>
2022-05-14 10:03:34 -0400

    Create SwarmManager and initialize connectivity levels - SwarmManager controls the bots - Three connectivity levels: Fully Connected, Partially Connected, Centralized

--- e2e_tests/test_e2e.py
+++ e2e_tests/test_e2e.py
@@ -1,27 +1,28 @@
 import logging
 import unittest
 
-from control_centre.src.control_centre import ControlCentre
 from swarm_bot.src.swarm_bot import SwarmBot
 from http_mock.src.http_mock import HttpMock
+from swarm_manager.src.swarm_manager import SwarmManager
+from swarm_manager.src.swarm_connectivity_level import SwarmConnectivityLevel
 
 class TestE2e(unittest.TestCase):
     def test_data_flows_can_be_assigned_to_sensors(self):
-        test_http_mock = HttpMock()
-        test_control_centre = ControlCentre(test_http_mock)
+        test_swarm_manager = SwarmManager(SwarmConnectivityLevel.FULLY_CONNECTED)
 
-        test_swarm_bot = SwarmBot(test_http_mock)
-        test_swarm_bot_id = test_swarm_bot.get_id()
+        test_swarm_bot_1 = SwarmBot()
+        test_swarm_bot_2 = SwarmBot()
 
-        test_control_centre.add_swarm_bot(test_swarm_bot)
+        test_swarm_manager.add_swarm_bot(test_swarm_bot_1)
+        test_swarm_manager.add_swarm_bot(test_swarm_bot_2)
 
         test_sensor_id = "SENSOR_NAME"
-        test_swarm_bot.add_sensor(test_sensor_id)
+        test_swarm_bot_1.add_sensor(test_sensor_id)
 
-        test_control_centre.define_data_flow(test_swarm_bot_id, test_sensor_id, [test_control_centre.get_id()])
+        test_swarm_bot_1.define_data_flow(test_sensor_id, [test_swarm_bot_2.get_id()])
 
-        expected_val = test_swarm_bot.read_from_sensor("SENSOR_NAME")
-        actual_val = test_control_centre.read_from_memory(test_swarm_bot_id, test_sensor_id)[0]
+        expected_val = test_swarm_bot_1.read_from_sensor("SENSOR_NAME")
+        actual_val = test_swarm_bot_2.read_from_memory(test_swarm_bot_1.get_id(), test_sensor_id)[0]
         self.assertEqual(expected_val, actual_val)
         
 
--- swarm_bot/src/swarm_bot.py
+++ swarm_bot/src/swarm_bot.py
@@ -4,29 +4,54 @@ from random import randint
 
 
 class SwarmBot(HttpCommunicator):
-    def __init__(self, http_mock):
+    def __init__(self):
         self.id = id(self)
 
-        self.http_mock = http_mock
-        self.http_mock.add_http_communicator(self)
+        self.http_mock = None
+
+        self.swarm_bots = []
         
         self.curr_task = None
         self.run_logs = []
 
         self.sensors = {}
 
+        self.memory = {}
+
+    def set_http_mock(self, new_http_mock):
+        self.http_mock = new_http_mock
+        self.http_mock.add_http_communicator(self)
+
     def get_id(self):
         return self.id
+
+    def define_data_flow(self, sensor_id, data_flow):
+        for bot_id in data_flow:
+            if bot_id not in self.swarm_bots:
+                raise Exception("ERROR: unknown swarm bot: " + str(bot_id))
+        self.sensors[sensor_id] = data_flow
+
+    def connect_to_swarm_bot(self, new_swarm_bot):
+        if not isinstance(new_swarm_bot, SwarmBot):
+            raise Exception("ERROR: new_swarm_bot must be of type SwarmBot")
+        bot_id = new_swarm_bot.get_id()
+        if not bot_id in self.swarm_bots:
+            self.swarm_bots.append(bot_id)
 
     def execute_curr_task(self, num_iterations):
         for _ in range(num_iterations):
             self.run_logs.append(self.curr_task())
 
     def receive_message(self, sender_id, message_type, message_payload):
-        if message_type == MessageTypes.NEW_DATA_FLOW:
+        if message_type == MessageTypes.SENSOR_VAL:
+            sender_id = sender_id
             sensor_id = message_payload["SENSOR_ID"]
-            data_flow = message_payload["DATA_FLOW"]
-            self.sensors[sensor_id] = data_flow
+            data = message_payload["DATA"]
+            if sender_id not in self.memory:
+                self.memory[sender_id] = {}
+            if sensor_id not in self.memory[sender_id]:
+                self.memory[sender_id][sensor_id] = []
+            self.memory[sender_id][sensor_id].append(data)
         else:
             raise Exception("ERROR: Unknown or undandled message type: " + str(message_type))
 
@@ -45,3 +70,7 @@ class SwarmBot(HttpCommunicator):
             else:
                 self.send_message(id, MessageTypes.SENSOR_VAL, {"SENSOR_ID": sensor_id, "DATA": new_val})
         return new_val
+
+    def read_from_memory(self, swarm_bot_id, sensor_id):
+        print(self.memory)
+        return self.memory[swarm_bot_id][sensor_id]
